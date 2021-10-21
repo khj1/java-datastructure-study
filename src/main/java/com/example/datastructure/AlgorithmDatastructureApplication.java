0790cd1f3200d969f6b499caf0e78e92e21a8d41
khj1 <74335027+khj1@users.noreply.github.com>
2021-10-21 23:52:39 +0900

    init

---
+++ src/main/java/com/example/datastructure/AlgorithmDatastructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDatastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmDatastructureApplication.class, args);
+    }
+
+}
